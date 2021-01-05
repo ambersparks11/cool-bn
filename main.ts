ee7a464682a6e3fc6f1720313044ee5964315436
ambersparks11 <76915207+ambersparks11@users.noreply.github.com>
2021-01-05 14:11:49 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,9 @@
 namespace SpriteKind {
     export const helper = SpriteKind.create()
     export const boss = SpriteKind.create()
+    export const sword = SpriteKind.create()
+    export const weaaak = SpriteKind.create()
 }
-/**
- * part1 mystery guy: waliking twords chairs suddenly voice says who dares to enter  my castle? 
- * 
- * part2 gal:umm me?  mysty guy: wrong answer!!!  *jumps down from rafters* raaaah words slash across screen bosssssss
- */
 function cutsean_1 () {
     gal.setPosition(208, 256)
     story.queueStoryPart(function () {
@@ -14,8 +11,7 @@ function cutsean_1 () {
         effects.starField.startScreenEffect(1000)
     })
     story.queueStoryPart(function () {
-        story.spriteSayText(gal, "us!", 9, 6, story.TextSpeed.Fast)
-        story.spriteSayText(boy, "us!", 2, 4, story.TextSpeed.Fast)
+        story.spriteSayText(gal, "me!!!!", 9, 6, story.TextSpeed.Fast)
     })
     story.queueStoryPart(function () {
         story.printText("then you better back up he he...", 1296, 988, 2, 15, story.TextSpeed.Normal)
@@ -53,12 +49,12 @@ function cutsean_1 () {
             ff777f77ffff7777ffffff7777f...
             f7777f7777777777777ffff777f...
             f777ff77777777777777fff777f...
-            f777f7777fff777ff7777ff777f...
-            7777f777777fffff77777ff777f...
-            7f7ff777777f77f7ff777ff777fff.
-            7f7f7777ffffccfff7777ff777777f
-            ff7f777777ffffff77f77ff777f77f
-            ..ff777ff77777777ff77ff7f7ff7f
+            f777f7777f222222f7777ff777f...
+            7777f7777721111277777ff777f...
+            7f7ff77777212212ff777ff777fff.
+            7f7f7777ff212212f7777ff777777f
+            ff7f77777721111277f77ff777f77f
+            ..ff777ff72222227ff77ff7f7ff7f
             ...f7777ff777777ff777f.ff7f.f.
             ...f777f7ffffffff7777f...f....
             ...ffffeeeeeeeeeeeeeeef.......
@@ -75,28 +71,54 @@ function cutsean_1 () {
             f77777777777f...f777777777ffff
             ffffffffffffff...fffffffffffff
             `, SpriteKind.boss)
+        bullseye = sprites.create(img`
+            1 1 1 1 1 1 1 1 
+            1 2 2 2 2 2 2 1 
+            1 2 1 1 1 1 2 1 
+            1 2 1 2 2 1 2 1 
+            1 2 1 2 2 1 2 1 
+            1 2 1 1 1 1 2 1 
+            1 2 2 2 2 2 2 1 
+            1 1 1 1 1 1 1 1 
+            `, SpriteKind.weaaak)
+        tiles.setTilemap(tiles.createTilemap(hex`1e001e0002020101010000000001010101040101010100000000000001010101010602020103010101010101010101010101010101010101010101010101010101010101010505050501030101010101030101010101010101010101010101010505050501010505020101010101020505050505050505050101010101010501010000000005050505050505050500000000000001050502020201010501010000000000000c0101010c000000000000000001010501010101010501010000000000000c0101010c000000000000000001030501030101010501010000000000000c0101010c000000000000000001010501010100000000000000000000000c0101010c000000000000000001010501010100000000000000000000000c0107010c00000000000000000101010101010000010101010000000101010101010101010101000000000101010101010000010101010000000d01010101010101010d0100000000010101010101000001020201000000010101010101010101010100000000010101010101000001030201000000010101010101010101010100000000010101010101000001010101010101010101010d01010101010100000000000000000000000001010101010101010101010101010101010100000000000000000000000001010101000000010d010101010101010d010000000000000000000000000101010100000001010109090901010101010000000000000000000000000101010100000001010b0808080a0101010100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+            22...2222.........222222......
+            22.2..........................
+            ..........2.....2.............
+            ..........2.....2.............
+            .....2222.........222222...222
+            .....2...22.....222....2......
+            .....2....2.....2......2.2..2.
+            .....2....2.....2......2......
+            222222....2.....2......2......
+            ..2222...22.....22222..2......
+            .2....2.2.2222222...2..2......
+            .2....2.2...........2..2......
+            .2.22.2.2...........2..2......
+            .2.22.222...........2..2......
+            .2..................2..2222222
+            .2..................2.........
+            .2....222...........2.........
+            .2....2.2...........2.........
+            .2....2.2...........2.........
+            .222222.2222222222222.........
+            ..............................
+            ..............................
+            ..............................
+            ..............................
+            ..............................
+            ..............................
+            ..............................
+            ..............................
+            ..............................
+            ..............................
+            `, [myTiles.transparency16,sprites.dungeon.floorDark2,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile15,myTiles.tile22], TileScale.Sixteen))
         bossy.setPosition(208, 208)
+        bullseye.setPosition(208, 208)
+        story.printText("my weak spot is sooo hard to see (im such a liar)", 1296, 988, 2, 15, story.TextSpeed.Normal)
     })
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    bloop = sprites.create(img`
-        . . . . . 8 8 8 8 8 . . . . . . 
-        . . . 8 8 6 6 6 6 6 8 8 8 8 . . 
-        . . . 8 6 6 6 6 6 6 6 6 6 6 8 . 
-        . . 8 8 6 6 9 9 9 9 9 6 6 6 8 . 
-        . . 8 6 6 6 9 9 9 9 9 6 6 6 8 . 
-        . . 8 6 6 6 9 9 9 9 9 6 6 6 8 . 
-        . 8 8 6 6 9 9 9 9 9 9 6 6 6 8 . 
-        . 8 6 6 6 9 9 9 9 9 9 6 6 6 8 . 
-        . 8 6 6 9 9 9 9 9 9 9 6 6 6 8 . 
-        8 6 6 9 9 9 9 9 9 9 9 6 6 8 . . 
-        8 6 6 9 9 9 9 9 9 9 6 6 6 8 . . 
-        8 6 6 9 9 9 9 9 9 6 6 6 6 8 . . 
-        8 8 6 6 9 9 6 6 6 6 6 6 6 8 . . 
-        . 8 6 6 6 6 6 6 6 6 6 6 6 8 . . 
-        . . 8 8 6 6 6 6 6 6 6 8 8 8 . . 
-        . . . 8 8 8 8 8 8 8 8 8 . . . . 
-        `, SpriteKind.Projectile)
     gal.setImage(img`
         ................
         ................
@@ -116,15 +138,15 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         ....ffffffdd6...
         .....ffff6dd6...
         ..........66....
-        ..........88....
-        .........888888.
-        ........8866668.
-        ........8699968.
-        ........8669968.
-        ........8869688.
-        .........88888..
+        .........6666...
+        ..........89....
+        ..........98....
+        ..........99....
+        ..........8.....
+        ................
+        ................
         `)
-    bloop.follow(bounty_hunter)
+    pause(500)
     gal.setImage(img`
         . . . . . . f f f f . . . . . . 
         . . . . f f f 8 8 f f f . . . . 
@@ -143,8 +165,6 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . f f f f f f . . . . . 
         . . . . . f f . . f f . . . . . 
         `)
-    pause(5000)
-    bloop.destroy()
 })
 function boss () {
     music.playMelody("D E F E F D F E ", 500)
@@ -158,22 +178,22 @@ function kill_enemy () {
 scene.onOverlapTile(SpriteKind.Player, myTiles.tile6, function (sprite, location) {
     cutsean_1()
 })
-scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
-    bloop.destroy()
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.boss, function (sprite, otherSprite) {
+    otherSprite.destroy()
 })
-scene.onOverlapTile(SpriteKind.Player, myTiles.tile5, function (sprite, location) {
-    key_obtaind = true
-})
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.boss, SpriteKind.Enemy, function (sprite, otherSprite) {
     bounty_hunter.destroy(effects.rings, 500)
-    info.changeScoreBy(1)
+    info.changeScoreBy(5)
     kill_enemy()
 })
-let key_obtaind = false
-let bloop: Sprite = null
+/**
+ * part1 mystery guy: waliking twords chairs suddenly voice says who dares to enter  my castle? 
+ * 
+ * part2 gal:umm me?  mysty guy: wrong answer!!!  *jumps down from rafters* raaaah words slash across screen bosssssss
+ */
+let bullseye: Sprite = null
 let bossy: Sprite = null
 let boss_fight = false
-let boy: Sprite = null
 let bounty_hunter: Sprite = null
 let gal: Sprite = null
 gal = sprites.create(img`
@@ -246,27 +266,9 @@ tiles.setTilemap(tiles.createTilemap(hex`1e001e000202010101000000000101010104010
     ..............................
     ..............................
     `, [myTiles.transparency16,sprites.dungeon.floorDark2,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile15,myTiles.tile22], TileScale.Sixteen))
-game.showLongText("you are a water nyad and your brother is a fire elemental in the shape of a human", DialogLayout.Full)
-game.showLongText("and you are doing the best you can to survive but beware many wish to kill you good luck! to attack press A o((>ω< ))o watch out for hunters!", DialogLayout.Full)
+game.showLongText("you are a water nyad who has lost her powers", DialogLayout.Full)
+game.showLongText("and you are doing the best you can to survive but beware many wish to kill you good luck! to attack press A to o((>ω< ))o watch out for hunters!", DialogLayout.Full)
 info.setLife(5)
-boy = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f 5 5 5 5 5 5 f f f . . 
-    . . f f 5 2 2 2 2 2 2 5 5 f . . 
-    . . f 5 2 f f f f f f 2 5 f . . 
-    . . f f f f d d d d f f f f . . 
-    . f f 4 f 2 f 4 4 f 2 f 4 f f . 
-    . f 4 4 4 2 f 4 4 f 2 4 4 4 f . 
-    . . f 4 4 4 4 4 4 4 4 4 4 f . . 
-    . . . f 4 4 4 3 3 4 4 4 f . . . 
-    . . 4 4 f 2 2 2 2 2 2 f 4 4 . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.helper)
 info.setScore(0)
 controller.player1.moveSprite(gal)
 forever(function () {
@@ -278,6 +280,7 @@ forever(function () {
 })
 forever(function () {
     tiles.placeOnRandomTile(bossy, myTiles.tile22)
+    pause(5000)
 })
 forever(function () {
     if (boss_fight == true) {
@@ -290,6 +293,9 @@ forever(function () {
         }
     }
 })
+forever(function () {
+	
+})
 game.onUpdateInterval(500, function () {
-    boy.follow(gal, 50)
+	
 })
